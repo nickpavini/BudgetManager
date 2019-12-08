0d5ae2067804b60beb1c3c9dc48993e0febc0e05
nickpavini <nickpavini@gmail.com>
2019-12-07 16:16:29 -0800

    Finish New Budget except for Create Budget button and begin work on edit budget Design.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -18,47 +18,58 @@ using System.Windows.Shapes;
 
 namespace BudgetManager
 {
+
+    enum Page { Main, NewBudget, EditBudget, AnalyzeBudget }; //refernece to keep track of which page we are on
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
+        public MainWindow() //Constructor
         {
             InitializeComponent();
 
-            InitializeMainMenu(); //set gui to main menu options
+            LoadPageMainMenu(); //set gui to main menu options
         }
 
-        //hides all app components except the main menu
-        private void InitializeMainMenu()
+        private void LoadPageMainMenu() //hides all app components except the main menu
         {
             btnAnalyzeBudget.Visibility = Visibility.Visible;
             btnEditBudget.Visibility = Visibility.Visible;
             btnNewBudget.Visibility = Visibility.Visible;
 
-            //hide new budget info gui
-            lblBudgetDir.Visibility = Visibility.Hidden;
-            lblBudgetName.Visibility = Visibility.Hidden;
-            txtBudgetName.Visibility = Visibility.Hidden;
-            txtBudgetDir.Visibility = Visibility.Hidden;
-            btnFileExplorer.Visibility = Visibility.Hidden;
-            btnCreateBudget.Visibility = Visibility.Hidden;
+            HidePageNewBudget();
 
             //hide back button
             btnBackButton.Visibility = Visibility.Hidden;
+
+            //set current and prev pages to main
+            pgCurPage = Page.Main;
+            pgPrevPage = Page.Main;
         }
 
-        #region Everything related to New Budget Page... I Will look into seperate files soon
-        //Load New Budget info GUI.
-        private void btnNewBudget_Click(object sender, RoutedEventArgs e)
+        private void HidePageMainMenu() //Hide Main Menu option
         {
-            //Hide Main Menu options
             btnAnalyzeBudget.Visibility = Visibility.Hidden;
             btnEditBudget.Visibility = Visibility.Hidden;
             btnNewBudget.Visibility = Visibility.Hidden;
+        }
 
-            //NewBudget info GUI Items
+        #region Everything related to New Budget Page... I Will look into seperate files soon
+        
+        private void btnNewBudget_Click(object sender, RoutedEventArgs e) //Load New Budget Page and hide Main Menu.
+        {
+            HidePageMainMenu();
+            LoadPageNewBudget();
+
+            //we are now on the New Budget Page
+            pgPrevPage = Page.Main;
+            pgCurPage = Page.NewBudget;
+        }
+
+        private void LoadPageNewBudget()  //Load NewBudget GUI Page items
+        {
             lblBudgetDir.Visibility = Visibility.Visible;
             lblBudgetName.Visibility = Visibility.Visible;
             txtBudgetName.Visibility = Visibility.Visible;
@@ -68,39 +79,94 @@ namespace BudgetManager
             btnBackButton.Visibility = Visibility.Visible;
         }
 
-        private void btnFileExplorer_Click(object sender, RoutedEventArgs e)
+        private void HidePageNewBudget() //Hide NewBudget GUI Page items
         {
-            //System.Diagnostics.Process.Start(Environment.GetEnvironmentVariable("WINDIR") + @"\explorer.exe", "/select, " + @"c:\Users\"); //starts windows processes with command line args
+            lblBudgetDir.Visibility = Visibility.Hidden;
+            lblBudgetName.Visibility = Visibility.Hidden;
+            txtBudgetName.Visibility = Visibility.Hidden;
+            txtBudgetDir.Visibility = Visibility.Hidden;
+            btnFileExplorer.Visibility = Visibility.Hidden;
+            btnCreateBudget.Visibility = Visibility.Hidden;
+        }
 
+        private void btnFileExplorer_Click(object sender, RoutedEventArgs e) //User directory selection choice
+        {
             //open dialog and allow user to select the folder of their choice to save
             using (var fbd = new FolderBrowserDialog())
             {
                 DialogResult result = fbd.ShowDialog();
 
                 if (result.ToString() == "OK" && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
-                {
-                    //string[] files = Directory.GetFiles(fbd.SelectedPath);
-                    //System.Windows.Forms.MessageBox.Show("Files found: " + files.Length.ToString(), "Message");
                     txtBudgetDir.Text = fbd.SelectedPath;
-                }
             }               
         }
 
-        //creates and saves the new budget
-        private void btnCreateBudget_Click(object sender, RoutedEventArgs e)
+        private void btnCreateBudget_Click(object sender, RoutedEventArgs e) //creates and saves the new budget then loads that budget into the editing page
         {
+            strBudgetPath = txtBudgetDir.Text + "\\" + txtBudgetName.Text + ".txt";
+            //DialogResult result = System.Windows.Forms.MessageBox.Show(strBudgetPath, "path", System.Windows.Forms.MessageBoxButtons.OK);
 
+            /*
+             * 
+             *  Logic for creating the actual budget
+             * 
+             */
+
+            btnBackButton.Visibility = Visibility.Hidden;
+            HidePageNewBudget();
+            LoadPageEditBudget();
+            pgCurPage = Page.EditBudget;
+            pgPrevPage = Page.NewBudget;
         }
+
+        private void btnBackButton_Click(object sender, RoutedEventArgs e)  //goes to last "page" (set of GUI options) we were on.
+        {
+            btnBackButton.Visibility = Visibility.Hidden;
+            HidePageNewBudget();
+            LoadPageMainMenu();
+
+            pgPrevPage = Page.NewBudget;
+            pgCurPage = Page.Main;
+        }
+
         #endregion
 
-        //goes to last "page" (set of GUI options) we were on.
-        private void btnBackButton_Click(object sender, RoutedEventArgs e)
-        {
+        #region Everything related to Edit Budget Page... I Will look into seperate files soon
 
+        private void btnEditBudget_Click(object sender, RoutedEventArgs e) // Choose file then load page and hide main menu page
+        {
+            // Open a file dialog to let them choose a file (.txt)
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "Text Files (*.txt)|*.txt";
+                DialogResult result = openFileDialog.ShowDialog();
+
+                if (result.ToString() == "OK" && !string.IsNullOrWhiteSpace(openFileDialog.FileName))
+                    strBudgetPath = openFileDialog.FileName; //full path to file
+                else if (result.ToString() == "Cancel")
+                    return; //stay on main menu
+            }
+            //DialogResult result1 = System.Windows.Forms.MessageBox.Show(strBudgetPath, "path", System.Windows.Forms.MessageBoxButtons.OK);
+
+            HidePageMainMenu();
+            LoadPageEditBudget();
+            pgPrevPage = Page.Main;
+            pgCurPage = Page.EditBudget;
+        }
+
+        private void LoadPageEditBudget()
+        {
+            //budget we are working with can be accessed with strBudgetPath
         }
 
 
+        #endregion
+
         #region Member Variables
+
+        private Page pgCurPage;
+        private Page pgPrevPage;
+        private string strBudgetPath;
 
         #endregion
     }
